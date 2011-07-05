1b8a6d7e4189534dbaa9bee62585a13969656393
lordzurp <zurp.online@gmail.com>
2011-07-05 15:15:36 +0200

    added transmission install script

--- scripts/userland/X.sh
+++ scripts/userland/X.sh
@@ -4,11 +4,8 @@
 # Xorg & xfce
 pkg_add -r xorg xorg-drivers liberation-fonts-ttf xfce4
 
-# X utils
-pkg_add -r bitstream-vera
-
 # X packages
-pkg_add -r chromium xfe xfce4-media thunar-volman leafpad gedit
+pkg_add -r bitstream-vera chromium xfe xfce4-media thunar-volman leafpad gedit
 
 # Configure X
 cd /root/
@@ -21,3 +18,7 @@ fetch https://raw.github.com/lordzurp/Zurpatator2/master/misc/x11-input.fdi
 echo 'setenv LANG fr_FR.UTF-8' >> /etc/csh.login
 echo 'setenv MM_CHARSET UTF-8' >> /etc/csh.login
 echo 'setenv LC_ALL fr_FR.UTF-8' >> /etc/csh.login
+
+# installe les drivers Nvidia (g210 inside)
+cd /tmp
+tar xzf NVIDIA-FreeBSD-x86_64-275.09.07.tar.gz && cd NVIDIA-FreeBSD-x86_64-275.09.07 && make install
