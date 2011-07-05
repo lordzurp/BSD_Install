1df46ab3c082d4cb9cfdd3657edd2d506a9569ac
lordzurp <zurp.online@gmail.com>
2011-07-05 17:00:01 +0200

    virtualbox misc

--- scripts/userland/virtualbox.sh
+++ scripts/userland/virtualbox.sh
@@ -12,23 +12,14 @@ echo "add path 'usb/*' mode 0660 group operator" >> /etc/devfs.rules
 # configure le service dans /etc/rc.conf
 echo '# VirtualBox' >> /etc/rc.conf
 echo 'vboxservice_enable="YES"' >> /etc/rc.conf
-echo 'vboxtoolinit_enable="YES"' >> /etc/rc.conf
 echo 'vboxnet_enable="YES"' >> /etc/rc.conf
+echo 'vboxheadless_enable="YES"' >> /etc/rc.conf
+echo 'vboxheadless_machines="HomeBack"' >> /etc/rc.conf
+echo 'vboxheadless_HomeBack_name="HomeBack"' >> /etc/rc.conf
 #echo 'vboxguest_enable="YES"' >> /etc/rc.conf
 #echo 'vboxwebsrv_enable="YES"' >> /etc/rc.conf
 #echo 'devfs_system_ruleset="system"' >> /etc/rc.conf
 echo '' >> /etc/rc.conf
-
-# vboxtool
-cd /usr/local/bin
-fetch https://raw.github.com/lordzurp/Zurpatator2/master/scripts/vboxtool/vboxtool
-chmod +x vboxtool
-cd /usr/local/etc/rc.d
-fetch https://raw.github.com/lordzurp/Zurpatator2/master/scripts/vboxtool/vboxtoolinit
-chmod +x vboxtoolinit
-mkdir /usr/local/etc/vboxtool
-echo "HomeBack" > /usr/local/etc/vboxtool/machines.conf
-echo "vbox_user='vboxusers'" > /usr/local/etc/vboxtool/vboxtool.conf
 
 # installe le service avahi
 cd /usr/local/etc/avahi/services
