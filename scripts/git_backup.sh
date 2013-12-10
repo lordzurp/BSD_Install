99ff6a9dccf47379d1531161b4f5dcb5f199ddde
lordzurp <zurp.online@gmail.com>
2013-12-11 00:09:09 +0100

    test from textmate2

--- scripts/git_backup.sh
+++ scripts/git_backup.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
 
+# Install git, obviously
+pkg install git
+pkg install hub
+
+# alias hub to git to enlarge your superpower
+echo "alias git hub" >> /root/.cshrc
+
+git config --global hub.protocol https
+git config --global user.name 'lordzurp'
+git config --global user.email 'zurp.online@gmail.com'
+
+git create $git_backup
