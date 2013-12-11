066e1c84b93f6d8b7985dc1ce64107b25282e057
lordzurp <zurp.online@gmail.com>
2013-12-11 01:17:02 +0100

    git backup

--- scripts/git_backup.sh
+++ scripts/git_backup.sh
@@ -11,4 +11,11 @@ git config --global hub.protocol https
 git config --global user.name 'lordzurp'
 git config --global user.email 'zurp.online@gmail.com'
 
+cd /
+git init
+git add /etc/rc.conf
+git add /etc/sysctl.conf
+git add /boot/loader.conf
+git commit -m 'inital commit'
+
 git create $git_backup
