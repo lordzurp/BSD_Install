f62d3422db7c9416ca0fed0a9895c7afb2bccfb7
lordzurp <zurp.online@gmail.com>
2014-01-04 02:12:21 +0100

    ajout du cshrc dans la sync

--- usr/scripts/update_scripts.sh
+++ usr/scripts/update_scripts.sh
@@ -9,6 +9,8 @@ cd /usr/scripts
 svnlite checkout $source_install_svn/usr/scripts .
 chmod -R +x *
 
+svnlite checkout $source_install_svn/root /root
+cp /root/cshrc /root/.cshrc
 
 echo " " >> /usr/scripts/journal.log
 echo " scripts mis à jour : " >> /usr/scripts/journal.log
