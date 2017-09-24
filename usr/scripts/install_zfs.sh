e5cfada70d52d32315d6e041f196904fb29972b6
Ti nicO <zurp.online@gmail.com>
2017-09-24 11:25:28 +0200

    upgrade to 11.1 Release
    some stuff to adjust new server (xeon, 10G card ...)

--- usr/scripts/install_zfs.sh
+++ usr/scripts/install_zfs.sh
@@ -48,275 +48,274 @@ fi
 
 # on detruit le disque 
 if [ ${erase_disc} = "YES" ];
-	then
-	
-	echo ''
-	echo '########################'
-	echo "# Effacement du pool ZFS"
-	echo '########################'
-	echo ''
-	
-	
-	# on nettoie le precedent pool
-	zpool import -f -R /mnt ${sys_tank}
-	zpool destroy -f ${sys_tank}
+then
 
-	zpool import -f -R /mnt ${jail_tank}
-	zpool destroy -f ${jail_tank}
+echo ''
+echo '########################'
+echo "# Effacement du pool ZFS"
+echo '########################'
+echo ''
 
-	zpool import -f -R /mnt ${data_tank}
-	zpool destroy -f ${data_tank}
-	
-	echo ''
-	echo '########################'
-	echo "# pool destroyed"
-	echo '########################'
-	echo ''
-	
-	
-	echo ''
-	echo '########################'
-	echo "# Erase du disque"
-	echo '########################'
-	echo ''
-	
-	gpart delete -i 1 ${disque_1}
-	gpart delete -i 2 ${disque_1}
-	gpart delete -i 3 ${disque_1}
-	gpart delete -i 4 ${disque_1}
-	gpart delete -i 5 ${disque_1}
-	gpart delete -i 6 ${disque_1}
-	gpart delete -i 7 ${disque_1}
-	gpart delete -i 8 ${disque_1}
-	gpart delete -i 9 ${disque_1}
-	gpart destroy -F ${disque_1}
-	echo ''
-	echo '########################'
-	echo "# disque effacé"
-	echo '########################'
-	echo ''
-	
+
+# on nettoie le precedent pool
+zpool import -f -R /mnt sys_tank
+zpool destroy -f sys_tank
+
+#zpool import -f -R /mnt ${jail_tank}
+#zpool destroy -f ${jail_tank}
+
+#zpool import -f -R /mnt ${data_tank}
+#zpool destroy -f ${data_tank}
+
+echo ''
+echo '########################'
+echo "# pool destroyed"
+echo '########################'
+echo ''
+
+
+echo ''
+echo '########################'
+echo "# Erase du disque"
+echo '########################'
+echo ''
+
+gpart delete -i 1 ${disque_1}
+gpart delete -i 2 ${disque_1}
+gpart delete -i 3 ${disque_1}
+gpart delete -i 4 ${disque_1}
+gpart delete -i 5 ${disque_1}
+gpart delete -i 6 ${disque_1}
+gpart delete -i 7 ${disque_1}
+gpart delete -i 8 ${disque_1}
+gpart delete -i 9 ${disque_1}
+gpart destroy -F ${disque_1}
+echo ''
+echo '########################'
+echo "# disque effacé"
+echo '########################'
+echo ''
+
 fi
 
 if [ ${partition_disc} = "YES" ];
-	then
-	echo ''
-	echo '########################'
-	echo "# Partition du disque"
-	echo '########################'
-	echo ''
-	
-	# création de la table GPT sur le disque 
-	gpart create -s gpt ${disque_1}
+then
+echo ''
+echo '########################'
+echo "# Partition du disque"
+echo '########################'
+echo ''
 
-	# création d une partition de boot, taille 512 secteurs
-	gpart add -s 512 -t freebsd-boot ${disque_1}
+# création de la table GPT sur le disque 
+gpart create -s gpt ${disque_1}
 
-	# création de la partition /, label system
-	gpart add -s ${partition_systeme} -t freebsd-zfs -l system ${disque_1}
+# création d une partition de boot, taille 512 secteurs
+gpart add -s 1m -t efi ${disque_1}
 
-	# création de la partition /jail, label jail
-	gpart add -s ${partition_jail} -t freebsd-zfs -l jail ${disque_1}
+# création de la partition /, label system
+gpart add -s ${partition_systeme} -t freebsd-zfs -l system ${disque_1}
 
-	# création de la partition /media, label data
-	if [ ${partition_data} != "NO" ]; then
-		gpart add -s ${partition_data} -t freebsd-zfs -l data ${disque_1}
-	fi
-	echo ''
-	echo '########################'
-	echo "# disque partitionné"
-	echo '########################'
-	echo ''
-	gpart show
-	
+# création de la partition /jail, label jail
+#gpart add -s ${partition_jail} -t freebsd-zfs -l jail ${disque_1}
+
+# création de la partition /media, label data
+if [ ${partition_data} != "NO" ]; then
+#gpart add -s ${partition_data} -t freebsd-zfs -l data ${disque_1}
+fi
+echo ''
+echo '########################'
+echo "# disque partitionné"
+echo '########################'
+echo ''
+gpart show
+
 fi
 
 if [ $create_pool = "YES" ];
-	then
+then
 
-	echo ''
-	echo '########################'
-	echo "# Creation du pool ZFS"
-	echo '########################'
-	echo ''
-		
-	# ça, c'etait dans le howto ... alors on laisse malgrè le warning ...
-	mkdir /boot/zfs
-	# on crée un dataset ZFS nommé ${sys_tank} sur la partition gpt/system
-	zpool create -f -R /mnt -m / ${sys_tank} /dev/gpt/system
-	zpool set feature@lz4_compress=enabled ${sys_tank}
-	zpool set listsnapshots=on zroot
-	zpool set autoreplace=on zroot
-	# on change le checksum
-	zfs set checksum=fletcher4 ${sys_tank}
-	# on active la deduplication
-	zfs set dedup=on ${sys_tank}
-	# on desactive la compression par défaut
-	zfs set compression=lz4 ${sys_tank}
-	zfs set atime=off ${sys_tank}
+echo ''
+echo '########################'
+echo "# Creation du pool ZFS"
+echo '########################'
+echo ''
 
-	# on export et réimporte le pool dans /mnt, en préservant zroot.cache dans /tmp
-	zpool export ${sys_tank}
-	zpool import -o cachefile=/tmp/zpool.cache -R /mnt ${sys_tank}
+# ça, c'etait dans le howto ... alors on laisse malgrè le warning ...
+mkdir /boot/zfs
+# on crée un dataset ZFS nommé sys_tank sur la partition gpt/system
+zpool create -f -R /mnt -m / sys_tank /dev/gpt/system
+zpool set feature@lz4_compress=enabled sys_tank
+zpool set listsnapshots=on sys_tank
+zpool set autoreplace=on sys_tank
+# on change le checksum
+zfs set checksum=fletcher4 sys_tank
+# on active la deduplication
+zfs set dedup=on sys_tank
+# on desactive la compression par défaut
+zfs set compression=lz4 sys_tank
+zfs set atime=off sys_tank
 
-	# on crée l'arboréscence ZFS
-	# on installe le système dans ${sys_tank}/root/current, ça permet de changer le /root si besoin (upgrade ...)
-	zfs create                                 ${sys_tank}/root
-	zfs create                                 ${sys_tank}/root/initial
-	zfs create                                 ${sys_tank}/usr
-	zfs create                                 ${sys_tank}/usr/home
-	zfs create                                 ${sys_tank}/usr/local
-	zfs create                 -o setuid=off   ${sys_tank}/usr/ports
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/usr/ports/distfiles
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/usr/ports/packages
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/usr/src
-	zfs create                                 ${sys_tank}/var
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/var/crash
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/var/db
-	zfs create -o exec=on      -o setuid=off   ${sys_tank}/var/db/pkg
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/var/empty
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/var/log
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/var/mail
-	zfs create -o exec=off     -o setuid=off   ${sys_tank}/var/run
-	zfs create -o exec=on      -o setuid=off   ${sys_tank}/var/tmp
-	zfs create -o exec=on      -o setuid=off   ${sys_tank}/tmp
+# on export et réimporte le pool dans /mnt, en préservant zroot.cache dans /tmp
+zpool export sys_tank
+zpool import -o cachefile=/tmp/zpool.cache -R /mnt sys_tank
 
-#	zfs create -o compression=on    -o exec=on      -o setuid=off	-o dedup=off   ${data_tank}
+# on crée l'arboréscence ZFS
+# on installe le système dans sys_tank/root/current, ça permet de changer le /root si besoin (upgrade ...)
+zfs create -o mountpoint=none                                 sys_tank/root
+zfs create -o mountpoint=/                                    sys_tank/root/initial
+zfs create -o mountpoint=/usr                                 sys_tank/usr
+zfs create                                                    sys_tank/usr/home
+zfs create                                                    sys_tank/usr/local
+zfs create                                    -o setuid=off   sys_tank/usr/ports
+zfs create                    -o exec=off     -o setuid=off   sys_tank/usr/ports/distfiles
+zfs create                    -o exec=off     -o setuid=off   sys_tank/usr/ports/packages
+zfs create                    -o exec=off     -o setuid=off   sys_tank/usr/src
+zfs create -o mountpoint=/var                                 sys_tank/var
+zfs create                    -o exec=off     -o setuid=off   sys_tank/var/crash
+zfs create                    -o exec=off     -o setuid=off   sys_tank/var/db
+zfs create                    -o exec=on      -o setuid=off   sys_tank/var/db/pkg
+zfs create                    -o exec=off     -o setuid=off   sys_tank/var/empty
+zfs create                    -o exec=off     -o setuid=off   sys_tank/var/log
+zfs create                    -o exec=off     -o setuid=off   sys_tank/var/mail
+zfs create                    -o exec=off     -o setuid=off   sys_tank/var/run
+zfs create                    -o exec=on      -o setuid=off   sys_tank/var/tmp
+zfs create -o mountpoint=/tmp -o exec=on      -o setuid=off   sys_tank/tmp
 
-	ln -s /mnt/home /usr/home
+#zfs create -o compression=on    -o exec=on      -o setuid=off-o dedup=off   ${data_tank}
+
+ln -s /usr/home /mnt/home
 
 
-	# on définit l'emplacement de la racine pour le boot
-	zpool set bootfs=${sys_tank}/root/initial ${sys_tank}
+# on définit l'emplacement de la racine pour le boot
+zpool set bootfs=sys_tank/root/initial sys_tank
 
-	# /var/empty en lecture seule
-	zfs set readonly=on ${sys_tank}/var/empty
+# /var/empty en lecture seule
+zfs set readonly=on sys_tank/var/empty
 
-	# /tmp en accès libre
-	chmod 1777 /mnt/tmp
-	chmod 1777 /mnt/var/tmp
+# /tmp en accès libre
+chmod 1777 /mnt/tmp
+chmod 1777 /mnt/var/tmp
 
-	# jail_tank
-	zpool create -f -R /mnt/jails ${jail_tank} /dev/gpt/jail
-	# on change le checksum
-	zfs set checksum=fletcher4 ${jail_tank}
-	zfs set dedup=on ${jail_tank}
-	zfs set compression=lz4 ${jail_tank}
-	zfs set atime=off ${jail_tank}
-	
-	# swap, sans dedup ni checksum
-	zfs create -V ${partition_swap} ${sys_tank}/swap
-	zfs set org.freebsd:swap=on ${sys_tank}/swap
-	zfs set checksum=off ${sys_tank}/swap
-	zfs set dedup=off ${sys_tank}/swap
-	
-	if [ ${partition_data} != "NO" ]; then
-		zpool create -f -R /mnt/media -m /media ${data_tank} /dev/gpt/data
-		# on change le checksum
-		zfs set checksum=fletcher4 ${data_tank}
-		# on desactive la deduplication
-		zfs set dedup=off ${data_tank}
-		# on desactive la compression par défaut
-		zfs set compression=off ${data_tank}
-		
-	fi
+# jail_tank
+#zpool create -f -R /mnt/jails ${jail_tank} /dev/gpt/jail
+# on change le checksum
+#zfs set checksum=fletcher4 ${jail_tank}
+#zfs set dedup=on ${jail_tank}
+#zfs set compression=lz4 ${jail_tank}
+#zfs set atime=off ${jail_tank}
 
-	# on umount le tout et on refait les points de montage propres
-	zfs umount -a
-	zfs set mountpoint=none ${sys_tank}
-	zfs set mountpoint=/ ${sys_tank}/root/initial
-	zfs set mountpoint=/usr ${sys_tank}/usr
-	zfs set mountpoint=/var ${sys_tank}/var
-	zfs set mountpoint=/tmp ${sys_tank}/tmp
-	
-	zfs set mountpoint=/usr/jails ${jail_tank}
-	zfs set mountpoint=/media ${data_tank}
+# swap, sans dedup ni checksum
+zfs create -V 2G sys_tank/swap
+zfs set org.freebsd:swap=on sys_tank/swap
+zfs set checksum=off sys_tank/swap
+zfs set dedup=off sys_tank/swap
 
-	echo ''
-	echo '########################'
-	echo "# pool ready"
-	echo '########################'
-	echo ''
-	zpool list
-	zfs list
-	
+if [ ${partition_data} != "NO" ]; then
+#zpool create -f -R /mnt/media -m /media ${data_tank} /dev/gpt/data
+# on change le checksum
+#zfs set checksum=fletcher4 ${data_tank}
+# on desactive la deduplication
+#zfs set dedup=off ${data_tank}
+# on desactive la compression par défaut
+#zfs set compression=off ${data_tank}
+
+fi
+
+
+#zfs set mountpoint=/usr/jails ${jail_tank}
+#zfs set mountpoint=/media ${data_tank}
+
+echo ''
+echo '########################'
+echo "# pool ready"
+echo '########################'
+echo ''
+zpool list
+zfs list
+
 
 fi
 
 # on export et importe le pool
-zpool export ${sys_tank}
-zpool export ${jail_tank}
-zpool export ${data_tank}
+zpool export sys_tank
+#zpool export ${jail_tank}
+#zpool export ${data_tank}
 
-zpool import -o cachefile=/tmp/zpool.cache -R /mnt ${sys_tank}
+zpool import -o cachefile=/tmp/zpool.cache -R /mnt sys_tank
 
 
 # on y va ?
 if [ ${valid_install} = "YES" ];
-	then
-	
-	echo ''
-	echo '########################'
-	echo "# Debut de l'install"
-	echo '########################'
-	echo ''
-	
-	
-	# Install de FreeBSD dans ${sys_tank}/root, monté sur /mnt
-#	mkdir /mnt/tmp/freebsd-dist
-#	cd /mnt/tmp/freebsd-dist
-#	fetch ${freebsd_install}/base.txz
-#	fetch ${freebsd_install}/lib32.txz
-#	fetch ${freebsd_install}/kernel.txz
-#	fetch ${freebsd_install}/doc.txz
-#	on n'installe pas les ports --> switch vers Subversion
-#	fetch ${freebsd_install}/ports.txz
-#	fetch ${freebsd_install}/src.txz
+then
 
-	echo ''
-	echo '########################'
-	echo "# Expand des paquets"
-	echo '########################'
-	echo ''
+echo ''
+echo '########################'
+echo "# Debut de l'install"
+echo '########################'
+echo ''
 
-	cd /usr/freebsd-dist
-	export DESTDIR=/mnt
-	for file in base.txz lib32.txz kernel.txz doc.txz ports.txz src.txz;
-	do (cat $file | tar --unlink -xpJf - -C ${DESTDIR:-/}); done
 
-	cd /tmp
-	
-	# on remet le cache zfs
-	cp /tmp/zpool.cache /mnt/boot/zfs/zpool.cache
-	
-	# bootcode zfs
-	gpart bootcode -b /mnt/boot/pmbr -p /mnt/boot/gptzfsboot -i 1 ${disque_1}
+# Install de FreeBSD dans sys_tank/root/initial, monté sur /mnt
+mkdir /mnt/tmp/freebsd-dist
+cd /mnt/tmp/freebsd-dist
+fetch ${freebsd_install}/base.txz
+fetch ${freebsd_install}/lib32.txz
+fetch ${freebsd_install}/kernel.txz
+#on n'installe pas les ports --> switch vers Subversion
+fetch ${freebsd_install}/ports.txz
+fetch ${freebsd_install}/src.txz
 
-	# Installe fstab, rc.conf sysctl.conf, make.conf et loader.conf, après backup
-	touch /mnt/etc/fstab
+echo ''
+echo '########################'
+echo "# Expand des paquets"
+echo '########################'
+echo ''
 
-	########################
-	### /root/.ssh/ma_cle_ssh.pub
-	########################
-	mkdir /mnt/root
-	mkdir /mnt/root/.ssh
-	echo ${ma_cle_ssh} > /mnt/root/.ssh/authorized_keys
+cd /mnt
+tar xvJpf /mnt/tmp/freebsd-dist/base.txz
+tar xvJpf /mnt/tmp/freebsd-dist/lib32.txz
+tar xvJpf /mnt/tmp/freebsd-dist/kernel.txz
+tar xvJpf /mnt/tmp/freebsd-dist/ports.txz
+tar xvJpf /mnt/tmp/freebsd-dist/src.txz
+
+#cd /usr/freebsd-dist
+#export DESTDIR=/mnt
+#for file in base.txz lib32.txz kernel.txz doc.txz ports.txz src.txz;
+#do (cat $file | tar --unlink -xpJf - -C ${DESTDIR:-/}); done
+
+cd /tmp
+
+# on remet le cache zfs
+cp /tmp/zpool.cache /mnt/boot/zfs/zpool.cache
+
+# bootcode zfs
+#gpart bootcode -b /mnt/boot/pmbr -p /mnt/boot/gptzfsboot -i 1 ${disque_1}
+
+# Installe fstab, rc.conf sysctl.conf, make.conf et loader.conf, après backup
+touch /mnt/etc/fstab
+
+########################
+### /root/.ssh/ma_cle_ssh.pub
+########################
+mkdir /mnt/root
+mkdir /mnt/root/.ssh
+echo ${ma_cle_ssh} > /mnt/root/.ssh/authorized_keys
 
 
 
  
-	# version moderne
-	#svnlite checkout $source_install_svn /mnt
-	# svnlite checkout https://www.github.com/lordzurp/BSD_Install/trunk /tmp
-	https://github.com/lordzurp/BSD_Install.git/trunk
-	cd /tmp
-	fetch --no-verify-peer https://github.com/lordzurp/BSD_Install/archive/master.zip
-	unzip master.zip
-	cp -R BSD_Install-master/* /mnt
+# version moderne
+#svnlite checkout $source_install_svn /mnt
+# svnlite checkout https://www.github.com/lordzurp/BSD_Install/trunk /tmp
+#https://github.com/lordzurp/BSD_Install.git/trunk
+cd /tmp
+fetch --no-verify-peer https://github.com/lordzurp/BSD_Install/archive/master.zip
+unzip master.zip
+cp -R BSD_Install-master/* /mnt
 
-	# on clean $sys_tank/tmp, il est monté en ram par rc.conf
-	zfs destroy -f ${sys_tank}/tmp
-	
+# on clean $sys_tank/tmp, il est monté en ram par rc.conf
+zfs destroy -f sys_tank/tmp
+
 fi
 
 
@@ -340,12 +339,12 @@ echo ''
 
 if [ ${auto_reboot} = "YES" ];
 then
-	echo " 20sec avant reboot"
-	sleep 20
-	echo "time for reboot :)"
-	shutdown -r now
+echo " 20sec avant reboot"
+sleep 20
+echo "time for reboot :)"
+shutdown -r now
 else
-	echo "rebootez maintenant :)"
-	echo "et pensez à remettre votre Kimsufi en boot Hard Disk !!"
+echo "rebootez maintenant :)"
+echo "et pensez à remettre votre Kimsufi en boot Hard Disk !!"
 fi
 
