b7752ee662a43af0d96b87dc8b48fd4d5a7d20ee
Ti nicO <zurp.online@gmail.com>
2017-09-26 07:24:08 +0200

    samba4.6 upgrade, work in progress

--- usr/scripts/userland/samba.sh
+++ usr/scripts/userland/samba.sh
@@ -1,10 +1,10 @@
 # Samba
-pkg_add -r samba35 samba35-libsmbclient pam_smb
+pkg install samba46 
 
 # installe les fichiers de config
 cd /usr/local/etc
-mv smb.conf smb.conf.dist
-fetch https://raw.github.com/lordzurp/Zurpatator2/master/usr_conf/smb.conf
+mv smb4.conf smb4.conf.dist
+fetch https://raw.github.com/lordzurp/Zurpatator2/master/usr/local/etc/smb4.conf
 chmod 644 smb.conf
 
 # configure le service dans /etc/rc.conf
@@ -14,16 +14,16 @@ echo '' >> /etc/rc.conf
 read -p "pensez à ajouter les membres des groupes 1000 et 1001 dans samba via webmin :)"
 
 # Configure le système
-chmod 644 smb.conf
+chmod 644 smb4.conf
 cd /etc
-ln -s ../usr/local/etc/smb.conf
+ln -s ../usr/local/etc/smb4.conf
 ln -s ..//usr/local/etc/samba
 cd rc.d
 ln -s ../../usr/local/etc/rc.d/samba
 
 # installe le service dans avahi
 cd /usr/local/etc/avahi/services
-fetch https://raw.github.com/lordzurp/Zurpatator2/master/usr_conf/avahi.services/service.samba.service
+fetch https://raw.github.com/lordzurp/Zurpatator2/master/usr/local/misc/avahi.services/service.samba.service
 
 # redémarre avahi-daemon
 /usr/local/etc/rc.d/avahi-daemon restart
