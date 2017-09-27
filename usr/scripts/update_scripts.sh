d7ddcc0f4a220ccd4c38d97165b049324957a42f
Ti nicO <zurp.online@gmail.com>
2017-09-27 18:11:15 +0200

    typo ...

--- usr/scripts/update_scripts.sh
+++ usr/scripts/update_scripts.sh
@@ -9,7 +9,7 @@ cd /usr/scripts
 svnlite checkout ${source_install_svn}/usr/scripts .
 chmod -R +x *
 
-svnlite checkout {$source_install_svn}/root /root
+svnlite checkout ${source_install_svn}/root /root
 cp /root/cshrc /root/.cshrc
 
 echo " " >> /usr/scripts/journal.log
