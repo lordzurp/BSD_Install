96ce3695e100e214f9c09daa90eabce5c649436e
Ti nicO <zurp.online@gmail.com>
2017-09-28 07:41:20 +0200

    X + Gnome3 install

--- usr/scripts/userland/X.sh
+++ usr/scripts/userland/X.sh
@@ -1,11 +1,11 @@
 ########################
 ### Install X server ###
 ########################
-# Xorg & xfce
-pkg_add -r xorg xorg-drivers liberation-fonts-ttf xfce4
+# Xorg & Gnome
+pkg install -y xorg xorg-drivers xf86-video-intel liberation-fonts-ttf gnome3
 
 # X packages
-pkg_add -r bitstream-vera chromium xfe xfce4-media thunar-volman leafpad gedit
+pkg install -y bitstream-vera chromium firefox leafpad linux-sublime3
 
 # Configure X
 cd /root/
@@ -13,12 +13,11 @@ X -configure
 mv xorg.conf.new /etc/X11/xorg.conf
 
 # Localisation
-cd /usr/local/etc/hal/fdi/policy/
-fetch https://raw.github.com/lordzurp/Zurpatator2/master/misc/x11-input.fdi
+#fetch https://raw.github.com/lordzurp/Zurpatator2/master/misc/x11-input.fdi
 echo 'setenv LANG fr_FR.UTF-8' >> /etc/csh.login
 echo 'setenv MM_CHARSET UTF-8' >> /etc/csh.login
 echo 'setenv LC_ALL fr_FR.UTF-8' >> /etc/csh.login
 
 # installe les drivers Nvidia (g210 inside)
 cd /tmp
-tar xzf NVIDIA-FreeBSD-x86_64-275.09.07.tar.gz && cd NVIDIA-FreeBSD-x86_64-275.09.07 && make install
+# tar xzf NVIDIA-FreeBSD-x86_64-275.09.07.tar.gz && cd NVIDIA-FreeBSD-x86_64-275.09.07 && make install
