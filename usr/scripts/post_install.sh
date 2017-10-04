ab291c0c0db8fe46954f99c19afc6c71ff2da346
Ti nicO <zurp.online@gmail.com>
2017-10-04 09:33:57 +0200

    dummy commit

--- usr/scripts/post_install.sh
+++ usr/scripts/post_install.sh
@@ -85,8 +85,8 @@ if [ ${system_install} = "YES" ]; then
 	
 	# màj des sources
 	pkg install -y ca_root_nss #subversion
-	#rm -rf /usr/src
-	#svnlite checkout ${freebsd_svn_checkout}/${freebsd_current_release} /usr/src
+	rm -rf /usr/src
+	svnlite checkout ${freebsd_svn_checkout}/${freebsd_current_release} /usr/src
 	
 	
 	echo "CPUTYPE?=${cputype}" >> /etc/make.conf
